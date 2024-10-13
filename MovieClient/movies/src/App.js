3620695f4ebb8c76d82c34c7ac7cb100b359d193
janina280 <coceijanina@yahoo.com>
2024-10-13 11:51:53 +0300

    create Layout and Home

--- MovieClient/movies/src/App.js
+++ MovieClient/movies/src/App.js
@@ -1,32 +1,33 @@
-import './App.css';
-import api from './api/axiosConfig';
-import { useState, useEffect } from 'react';
+import "./App.css";
+import api from "./api/axiosConfig";
+import { useState, useEffect } from "react";
+import Layout from "./components/Layout";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/home/Home";
 
 function App() {
-
   const [movies, setMovies] = useState();
 
   const getMovies = async () => {
-    try
-     {
+    try {
       const response = await api.get("/api/v1/movies");
-     
-console.log(response.data);
 
       setMovies(response.data);
-    }
-    catch (err) 
-    {
+    } catch (err) {
       console.log(err);
     }
-  }
+  };
 
-  useEffect(()=>{
+  useEffect(() => {
     getMovies();
-  },[])
+  }, []);
   return (
     <div className="App">
-
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home />}></Route>
+        </Route>
+      </Routes>
     </div>
   );
 }
